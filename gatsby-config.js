481440e09b7d38572e160f736dc5fe0f89e6e381
katonobu <katonobu4649@gmail.com>
2022-01-13 01:18:40 +0900

    基本動作確認済。コンテナにサイズを指定したのがポイント。

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-react-leaflet',
+      options: {
+        linkStyles: true // (default: true) Enable/disable loading stylesheets via CDN
+      }
+    }    
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,72 @@
-import React from "react"
+import React, {useState, useMemo, useCallback, useEffect} from 'react'
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const center = [35.68, 139.76];
+const zoom = 13
+
+const DisplayPosition = ({ map }) => {
+  const [position, setPosition] = useState(map.getCenter())
+
+  const onClick = useCallback(() => {
+    map.setView(center, zoom)
+  }, [map])
+
+  const onMove = useCallback(() => {
+    setPosition(map.getCenter())
+  }, [map])
+
+  useEffect(() => {
+    map.on('move', onMove)
+    return () => {
+      map.off('move', onMove)
+    }
+  }, [map, onMove])
+
+  return (
+    <p>
+      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
+      <button onClick={onClick}>reset</button>
+    </p>
+  )
+}
+
+function MapPlaceholder() {
+  return (
+    <p>
+      Loading map....{' '}
+      <noscript>You need to enable JavaScript to see this map.</noscript>
+    </p>
+  )
+}
+
+export default function Index() {
+  const [map, setMap] = useState(null)
+  const displayMap = useMemo(
+    () => (
+      <MapContainer
+        center={center}
+        zoom={zoom}
+        scrollWheelZoom={true}
+        whenCreated={setMap}
+        placeholder={<MapPlaceholder />}
+        style={{width: '90vw', height: '90vh', margin: '10px'}}>        
+        <TileLayer
+          attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
+          url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+        />      
+        <Marker position={center}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+      </MapContainer>
+    ),
+    [],
+  )            
+  return (
+    <div>
+      {map ? <DisplayPosition map={map} /> : null}        
+      {displayMap}
+    </div>
+  )
 }
