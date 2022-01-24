6a5fce10f795c5c82157cebe8cceb84fbe412709
katonobu <katonobu4649@gmail.com>
2022-01-24 19:28:21 +0900

    geolocation 動作確認用にシンプルな構成に変更。

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,33 @@
 import React, {useState, useMemo, useCallback, useEffect} from 'react'
-import { MapContainer, LayersControl, TileLayer, Polygon, Polyline } from 'react-leaflet'
-import {higashitotukasho, maichu, totukaku, yoshidayabe} from "../components/lines"
-import {wangan} from '../components/wangan.js'
-import Hyakumeisan, {Markers} from '../components/hyakumeisan';
+import { MapContainer, LayersControl, TileLayer, Marker, Popup , Circle} from 'react-leaflet'
 
 const center = [35.3608333,138.7275];
 const zoom = 13
 
-const limeOptions = { color: 'lime' }
-const purpleOptions = { color: 'purple' }
-const redOptions = { color: 'red' }
+const GetCurrentPosition = ({setCurrentPos}) => {
+  const success = (position) => {
+    console.log(position)
+    setCurrentPos(position)
+  }
+
+  const error = ()=> {
+    console.log = 'Unable to retrieve your location';
+  }
+
+  const getPos = ()=>{
+    if(!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser');
+    } else {
+      console.log('Locating…')
+      navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true});
+    }
+  }
+  return (
+    <button onClick={getPos}>
+      GetCurrentPosition
+    </button>
+  )
+}
 
 const DisplayPosition = ({ map }) => {
   const [position, setPosition] = useState(map.getCenter())
@@ -27,7 +45,7 @@ const DisplayPosition = ({ map }) => {
 
   return (
     <p>
-      latitude: {position.lat.toFixed(4)}, longitude: {position.lng.toFixed(4)}{' '}
+      MapCenter [lat,lon]=[{position.lat.toFixed(4)},{position.lng.toFixed(4)}]
     </p>
   )
 }
@@ -42,79 +60,57 @@ function MapPlaceholder() {
 }
 
 export default function Index() {
+  const [currentPos, setCurrentPos] = useState({coords:{latitude:center[0], longitude:center[1], accuracy:0}})
   const [map, setMap] = useState(null)
-  const [index, setIndex] = useState(-1)
-  const [name, setName] = useState("")
+  const [getDate, setGetDate] = useState(null)
+  useEffect(()=>{
+    if (map) {
+      map.flyTo([currentPos.coords.latitude, currentPos.coords.longitude])
+      const timeStamp = new Date()
+      timeStamp.setTime(currentPos.timestamp)
+      setGetDate(timeStamp)
+    }
+  }, [map, currentPos])
   const displayMap = useMemo(
     () => (
-      <MapContainer
-        center={center}
-        zoom={zoom}
-        scrollWheelZoom={true}
-        whenCreated={setMap}
-        placeholder={<MapPlaceholder />}
-        style={{width: '90vw', height: '90vh', margin: '10px'}}>
-        <LayersControl position="topright">
-          <LayersControl.BaseLayer checked name="地理院地図.標準">
-            <TileLayer
-              attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-              url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
-            />      
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="地理院地図.淡色">
-            <TileLayer
-              attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-              url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
-            />      
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="地理院地図.白地図">
-            <TileLayer
-              attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
-              url="https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png"
-            />      
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="OpenStreetMap.Mapnik">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.Overlay name="戸塚区">
-            <Polygon pathOptions={purpleOptions} positions={totukaku} />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="東戸塚小学区">
-            <Polygon pathOptions={limeOptions} positions={higashitotukasho} />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="舞岡中学区">
-            <Polygon pathOptions={redOptions} positions={maichu} />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="吉田矢部地区">
-            <Polygon pathOptions={redOptions} positions={yoshidayabe} />
-          </LayersControl.Overlay>
-          <LayersControl.Overlay name="湾岸道路">
-            <Polyline  pathOptions={redOptions} positions={wangan} />
-          </LayersControl.Overlay>
-        </LayersControl>
-        <Markers></Markers>
-      </MapContainer>
+      <>
+        {(center[0] !== currentPos.coords.latitude && center[1] !== currentPos.coords.longitude)?<p>CurrentPosition [lon,lat]=[{currentPos.coords.latitude},{currentPos.coords.longitude}]</p>:null}
+        {0 < currentPos.coords.accuracy?<p>Accuracy = {currentPos.coords.accuracy.toString()}[m]</p>:null}
+        {0 < currentPos.timestamp?<p>GetAt {getDate.toString()}</p>:null}
+        <MapContainer
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={true}
+          whenCreated={setMap}
+          placeholder={<MapPlaceholder />}
+          style={{width: '90vw', height: '90vh', margin: '10px'}}>
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name="地理院地図.標準">
+              <TileLayer
+                attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
+                url="https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png"
+              />      
+            </LayersControl.BaseLayer>
+            <LayersControl.BaseLayer name="地理院地図.淡色">
+              <TileLayer
+                attribution='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>'
+                url="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png"
+              />      
+            </LayersControl.BaseLayer>
+          </LayersControl>
+          <Marker position={[currentPos.coords.latitude, currentPos.coords.longitude]}>
+            <Popup>CurrentPosition</Popup>
+          </Marker>
+          {0 < currentPos.coords.accuracy? <Circle center={[currentPos.coords.latitude, currentPos.coords.longitude]} radius={currentPos.coords.accuracy}></Circle>:null}
+        </MapContainer>
+      </>
     ),
-    [],
+    [currentPos, getDate],
   )            
-  const onIndexChanged = useCallback((newIndex, newName) => {
-    setIndex(newIndex)
-    setName(newName)
-  }, [])
   return (
     <div>
       {map ? <DisplayPosition map={map} /> : null}        
-      {map ? <Hyakumeisan map={map} onIndexChanged={onIndexChanged}/> : null}        
-      <p>{(0<=index)?((index+1).toString() + ":" + name):""}</p>
+      {map ? <GetCurrentPosition setCurrentPos = {setCurrentPos}/> : null}        
       {displayMap}
     </div>
   )
